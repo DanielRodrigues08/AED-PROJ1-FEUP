95ca1a4e09c97ae6ed4f7d1ff69357101652a96f
DanielRodrigues202006562 <up202006562@up.pt>
2021-12-14 23:50:44 +0000

    update

---
+++ src/Services.cpp
@@ -0,0 +1,5 @@
+//
+// Created by danie on 14/12/2021.
+//
+
+#include "Services.h"
---
+++ src/Services.h
@@ -0,0 +1,16 @@
+//
+// Created by danie on 14/12/2021.
+//
+
+#ifndef AED_PROJ1_SERVICES_H
+#define AED_PROJ1_SERVICES_H
+
+
+enum Type{maintenance, cleaning};
+
+class Services {
+
+};
+
+
+#endif //AED_PROJ1_SERVICES_H
