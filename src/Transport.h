e57801af6ca3a21be1ba3b61d9a7b6e739e47881
DanielRodrigues202006562 <up202006562@up.pt>
2021-12-14 15:25:29 +0000

    BST, Update Time, Add Transport

---
+++ src/Transport.h
@@ -0,0 +1,26 @@
+#ifndef AED_PROJ1_TRANSPORT_H
+#define AED_PROJ1_TRANSPORT_H
+#include <string>
+#include <vector>
+#include "BST.h"
+#include "Time.h"
+
+using namespace std;
+
+enum Type {metro, train, bus};
+
+class Transport{
+    Type type;
+    string destination;
+    BST<Time> schedule;
+public:
+    Transport(string destination, Type type);
+    void addTime( Time t1);
+    void addTime(stack<Time>& t1);
+    bool removeTime(const Time& t1);
+    string getDestination() const;
+    void changeDestination( string d1);
+
+};
+
+#endif //AED_PROJ1_TRANSPORT_H
