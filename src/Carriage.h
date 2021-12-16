eb0daa8937e2d71cf95bb92bec15107ed75f471b
DanielRodrigues202006562 <up202006562@up.pt>
2021-12-16 11:51:46 +0000

    add coments to carriage

--- src/Carriage.h
+++ src/Carriage.h
@@ -12,13 +12,41 @@ class Carriage {
     unsigned maxNumSuitcases;
     unsigned maxNumStacks;
 public:
+    /**
+     * Default Constructor
+     */
     Carriage();
+    /**
+     * Constructor
+     * @param maxNumSuitcases Number of bags that fit in a stack
+     * @param maxNumStacks Number of stacks
+     */
     Carriage(unsigned maxNumSuitcases, unsigned maxNumStacks);
+    /**
+     * If possible add a suitcase to a stack
+     * @param l1 Suitcase we want to add
+     * @return True if the suitcase was added otherwise it returns false
+     */
     bool addSuitcase(const CheckedSuitcase& l1);
-    CheckedSuitcase removeSuitcase();
+    /**
+     * Determines carriage capacity
+     * @return carriage capacity
+     */
     unsigned getCapacity();
+    /**
+     * Check if the carriage is empty
+     * @return Returns true if carriage is empty otherwise returns false
+     */
     bool isEmpty();
+    /**
+     * Remove all bags from the carriage
+     * @return A list of all baggage
+     */
     list<CheckedSuitcase> unloadCarriage();
+    /**
+     * Load the bags to the carriage
+     * @param luggage bags to load
+     */
     void  loadSuitcases(stack<CheckedSuitcase> &luggage);
 };
 
